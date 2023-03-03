73d552dd0a7e452acae488fba31a45c8ad201b8c
Sofiia Shmaiun <sofiia.shmaiun@gmail.com>
2023-03-03 21:28:12 +0200

    second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const listOfImages = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    galleryItem =>
+      `<div class="gallery__item">
+      <a class="gallery__item" href="${galleryItem.original}">
+        <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}" />
+      </a>
+    </div>`
+  )
+  .join('');
+
+listOfImages.insertAdjacentHTML('afterbegin', markup);
+
+new SimpleLightbox('.gallery a', {captionType: 'attr', captionsData: 'alt', captionDelay: 250});
