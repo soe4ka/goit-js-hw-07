f2d94c79c609f4e96e39c8f530d1284899a39e3d
Sofiia Shmaiun <sofiia.shmaiun@gmail.com>
2023-03-04 10:25:34 +0200

    first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const listOfImages = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    galleryItem =>
+      `<div class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+        <img
+          class="gallery__image"
+          src="${galleryItem.preview}"
+          data-source="${galleryItem.original}"
+          alt="${galleryItem.description}"
+        />
+      </a>
+    </div>`
+  )
+  .join('');
+
+listOfImages.insertAdjacentHTML('afterbegin', markup);
+
+listOfImages.addEventListener('click', handleOpenImage);
+
+function handleOpenImage(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${event.target.getAttribute('data-source')}">
+    <img
+      class="gallery__image"
+      src="${event.target.getAttribute('data-source')}"
+      data-source="${event.target.getAttribute('data-source')}"
+      alt="${event.target.getAttribute('alt')}"
+    />
+  </a>
+  </div>`
+  );
+
+  instance.show();
+}
